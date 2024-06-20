4db53f9cb1366e10d109e926e0fd100ddfd214e5
Gabriel de Oliveira Lopes <lopesgabriel0199@gmail.com>
2024-06-20 10:27:17 -0300

    initial commit

---
+++ Dockerfile
@@ -0,0 +1,25 @@
+FROM node:18 AS build
+
+WORKDIR /usr/src/app
+
+COPY package*.json .
+RUN npm i
+
+COPY . .
+RUN npm run build
+RUN npm prune --production
+
+FROM node:18-alpine3.19
+
+WORKDIR /usr/src/app
+
+RUN npm i -g prisma
+
+COPY --from=build /usr/src/app/dist ./dist
+COPY --from=build /usr/src/app/prisma ./prisma
+COPY --from=build /usr/src/app/package*.json .
+COPY --from=build /usr/src/app/node_modules ./node_modules
+
+EXPOSE 3000
+
+CMD [ "npm", "run", "start:prod" ]
---
+++ src/app.controller.ts
@@ -0,0 +1,41 @@
+import { Body, Controller, Get, HttpCode, Param, Post } from '@nestjs/common';
+import { DatabaseService } from './database.service';
+
+class NoteDto {
+  id?: string;
+  title: string;
+  content: string;
+  createdAt?: Date;
+  updatedAt?: Date;
+}
+
+@Controller()
+export class AppController {
+  constructor(private readonly databaseService: DatabaseService) {}
+
+  @Get()
+  async listNotes() {
+    const notes = await this.databaseService.note.findMany();
+
+    return notes;
+  }
+
+  @HttpCode(201)
+  @Post()
+  async createNote(@Body() noteDto: NoteDto) {
+    const note = await this.databaseService.note.create({
+      data: noteDto,
+    });
+
+    return note;
+  }
+
+  @Get(':noteId')
+  async getNote(@Param('noteId') noteId: string) {
+    const note = await this.databaseService.note.findUnique({
+      where: { id: noteId },
+    });
+
+    return note;
+  }
+}
