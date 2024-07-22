5dc967d8e2156d22f66b622711d0b70b217a94aa
Gabriel de Oliveira Lopes <lopesgabriel0199@gmail.com>
2024-07-22 15:03:21 -0300

    fix: fix permissions and api response

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,8 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World!';
+    const now = new Date();
+    const currentTime = now.toLocaleString(new Intl.Locale('pt-BR'));
+    return `Hello, Gabriel! The current time is ${currentTime}.`;
   }
 }
