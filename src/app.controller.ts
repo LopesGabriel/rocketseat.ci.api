7c281028b6d3662d81ce773550c9b7c888d327fc
Gabriel de Oliveira Lopes <lopesgabriel0199@gmail.com>
2024-07-22 13:59:33 -0300

    fix: removes database dependency

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,48 +1,50 @@
-import { Body, Controller, Get, HttpCode, Param, Post } from '@nestjs/common';
-import { DatabaseService } from './database.service';
+// import { Body, Controller, Get, HttpCode, Param, Post } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
+// import { DatabaseService } from './database.service';
 import { AppService } from './app.service';
 
-class NoteDto {
-  id?: string;
-  title: string;
-  content: string;
-  createdAt?: Date;
-  updatedAt?: Date;
-}
+// class NoteDto {
+//   id?: string;
+//   title: string;
+//   content: string;
+//   createdAt?: Date;
+//   updatedAt?: Date;
+// }
 
 @Controller()
 export class AppController {
   constructor(
-    private readonly databaseService: DatabaseService,
+    // private readonly databaseService: DatabaseService,
     private readonly appService: AppService,
   ) {}
 
+  // @Get()
+  // async listNotes() {
+  //   const notes = await this.databaseService.note.findMany();
+
+  //   return notes;
+  // }
+
+  // @HttpCode(201)
+  // @Post()
+  // async createNote(@Body() noteDto: NoteDto) {
+  //   const note = await this.databaseService.note.create({
+  //     data: noteDto,
+  //   });
+
+  //   return note;
+  // }
+
+  // @Get(':noteId')
+  // async getNote(@Param('noteId') noteId: string) {
+  //   const note = await this.databaseService.note.findUnique({
+  //     where: { id: noteId },
+  //   });
+
+  //   return note;
+  // }
+
   @Get()
-  async listNotes() {
-    const notes = await this.databaseService.note.findMany();
-
-    return notes;
-  }
-
-  @HttpCode(201)
-  @Post()
-  async createNote(@Body() noteDto: NoteDto) {
-    const note = await this.databaseService.note.create({
-      data: noteDto,
-    });
-
-    return note;
-  }
-
-  @Get(':noteId')
-  async getNote(@Param('noteId') noteId: string) {
-    const note = await this.databaseService.note.findUnique({
-      where: { id: noteId },
-    });
-
-    return note;
-  }
-
   getHello() {
     return this.appService.getHello();
   }
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { DatabaseModule } from './database.module';
-import { DatabaseService } from './database.service';
+// import { DatabaseModule } from './database.module';
+// import { DatabaseService } from './database.service';
 
 @Module({
-  imports: [DatabaseModule],
+  imports: [],
   controllers: [AppController],
-  providers: [AppService, DatabaseService],
+  providers: [AppService],
 })
 export class AppModule {}
